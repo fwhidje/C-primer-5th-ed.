40d2f5be17cc410af57d785ea7f6108e187eea15
Filip <filipweyn@hotmail.com>
2016-12-08 22:23:19 +0100

    Update Section 4.2.cpp

--- Section 4.2.cpp
+++ Section 4.2.cpp
@@ -2,23 +2,23 @@
 using namespace::std;
 
 //Exercise 4.4
-void Eval() {
+void eval() {
 	cout << 12 / 3 * 4 + 5 * 15 + 24 % 4 / 2 << endl;
 	cout << ((12 / 3) * 4) + (5 * 15) + ((24 % 4) / 2) << endl;
 }
 
 //Exercise 4.6
-bool IsOdd(const int i) {
+bool is_odd(const int i) {
 	return i % 2;
 }
 
  //Exercise 4.6 - variation
-bool IsOdd2(int i) {
+bool is_odd2(int i) {
 	return (i & 1);
 }
 
 //Exercise 4.7
-void Overflow() {
+void overflow() {
 	unsigned short i = 65535;
 	short j = i;
 	cout << "unsigned short i: " << i << ", short j = i: " << j << endl;
@@ -28,7 +28,7 @@ void Overflow() {
 
 
 int main() {
-	Eval();
-	cout << IsOdd2(3);
-	Overflow();
+	eval();
+	cout << is_odd2(3);
+	overflow();
 }
