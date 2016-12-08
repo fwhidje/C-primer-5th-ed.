12508b79233a83847a30336ba82821adff9ad22e
Filip <filipweyn@hotmail.com>
2016-12-08 22:26:50 +0100

    Update Section 3.5.3.cpp

--- Section 3.5.3.cpp
+++ Section 3.5.3.cpp
@@ -4,13 +4,13 @@ using namespace::std;
 
 
 //exercise 3.35
-void setToZero(int* b, int* e) {
+void set_to_zero(int* b, int* e) {
 	while (b != e) 
 		*b++ = 0;
 }
 
 //exercise 3.36
-bool checkEquality(const int* b1, const int* e1, const int* b2, const int* e2) {
+bool check_equality(const int* b1, const int* e1, const int* b2, const int* e2) {
 	if ((e1 - b1) != (e2 - b2))
 		return 0;
 	for (const int* t1 = b1, *t2 = b2; t1 != e1; ++t1, ++t2) {
