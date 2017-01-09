5d08d125a73512c5ca1d8861290ee3d453058831
Filip <filipweyn@hotmail.com>
2017-01-09 12:52:21 +0100

    Add files via upload

---
+++ Chapter 3/Section 3.5.3.cpp
@@ -0,0 +1,25 @@
+#include <iterator>
+#include <iostream>
+using namespace::std;
+
+
+//exercise 3.35
+void set_to_zero(int* b, int* e) {
+	while (b != e) 
+		*b++ = 0;
+}
+
+//exercise 3.36
+bool check_equality(const int* b1, const int* e1, const int* b2, const int* e2) {
+	if ((e1 - b1) != (e2 - b2))
+		return 0;
+	for (const int* t1 = b1, *t2 = b2; t1 != e1; ++t1, ++t2) {
+		if (*t1 != *t2)
+			return 0;
+	}
+	return 1;
+}
+
+int main() {
+	return 0;
+}
